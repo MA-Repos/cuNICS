b8d1eeff6ab4b545ca9528cd7bb854795d9e1caf
Troy <troyhavelock@cmail.carleton.ca>
2017-03-01 17:33:18 -0500

    Adding getter and setter methods for user class

--- TeamOOPS_cuNICS/user.cpp
+++ TeamOOPS_cuNICS/user.cpp
@@ -1,7 +1,58 @@
 #include "user.h"
+#include <string>
+
+using namespace std;
 
 User::User()
 {
-
+    User("None", "None", "None", "None");
 }
 
+User::User(string fName, string lName, string username, string password)
+{
+    this->fName = fName;
+    this->lName = lName;
+    this->username = username;
+    this->password = password;
+}
+
+//Setters
+bool User::setFName(string fName)
+{
+    this->fName = fName;
+    return true;
+}
+
+bool User::setLName(string lName)
+{
+    this->lName = lName;
+    return true;
+}
+
+bool User::setUsername(string username)
+{
+    this->username = username;
+    return true;
+}
+
+bool User::setPassword(string password)
+{
+    this->password = password;
+    return true;
+}
+
+//Getters
+string User::getFName()
+{
+    return fName;
+}
+
+string User::getLName()
+{
+    return lName;
+}
+
+string User::getUsername()
+{
+    return username;
+}
--- TeamOOPS_cuNICS/user.h
+++ TeamOOPS_cuNICS/user.h
@@ -1,11 +1,34 @@
 #ifndef USER_H
 #define USER_H
 
+#include <string>
+using namespace std;
 
 class User
 {
+protected:
+    string fName;
+    string lName;
+    string username;
+    string password;
+
 public:
     User();
+    User(string fName, string lName, string username, string password);
+
+    //Setters
+    bool setFName(string);
+    bool setLName(string);
+    bool setUsername(string);
+    bool setPassword(string);
+
+    //Getters
+    string getFName();
+    string getLName();
+    string getUsername();
+
+
+
 };
 
 #endif // USER_H
