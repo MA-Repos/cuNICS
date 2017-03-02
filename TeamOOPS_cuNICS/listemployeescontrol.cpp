35fd3cddb021d66b6ab4c69c87a8dfb339b06e5b
admin <yunwei.li@carleton,ca>
2017-03-01 20:31:31 -0500

    a few control objects

---
+++ TeamOOPS_cuNICS/listemployeescontrol.cpp
@@ -0,0 +1,18 @@
+#include "listemployeescontrol.h"
+
+ListEmployeesControl::ListEmployeesControl()
+{
+    this->employees = NULL;
+}
+
+int ListEmployeesControl::getAllEmployees() {
+
+    //this->employees
+    return 0;
+}
+
+int ::ListEmployeesControl::DisplayEmployeeList(){
+
+    return 0;
+}
+
