4143696004ef4bba01863470f3747e265ddca340
admin <yunwei.li@carleton,ca>
2017-03-05 15:02:16 -0500

    progress

--- TeamOOPS_cuNICS/db/sql.sql
+++ TeamOOPS_cuNICS/db/sql.sql
@@ -2,8 +2,11 @@ create table employee (
 employeeid    integer primary key autoincrement,
 firstname     char(20) not null,
 lastname      char(20) not null,
-sinnumber     integer not null unique check(sinnumber > 99999999 and sinnumber < 1000000000),
-phonenumber   char(20) ,
+sinnumber     char(9) not null unique,
+countrycode   char(7),
+areacode      char(7),
+localnumber   char(15),
+extension     char(5),
 street        char(20),
 streetnumber  integer,
 city          char(20),
@@ -23,6 +26,7 @@ employmenttype   char(20) check (employmenttype   in ('Term', 'Continuing')),
 startdate        date,
 enddate          date,
 salary           number check(salary > 0),
+deductionpercentage number   check (deductionpercentage >= 0 and deductionpercentage < 1),
 primary key (employeeid, roletype),
 foreign key (employeeid) references employee(employeeid));
 
@@ -33,23 +37,22 @@ paydate             date,
 grossincome         number   check (grossincome > 0),
 netincome           number   check (netincome > 0),
 taxdeductions       number   check (taxdeductions >= 0 and taxdeductions < 1),
-deductionpercentage number   check (deductionpercentage >= 0 and deductionpercentage < 1),
 ytdgrossincome      number   check (ytdgrossincome > 0),
 ytdnetincome        number   check (ytdnetincome > 0),
 ytdtaxdeductions    number   check (ytdtaxdeductions > 0),
 primary key (employeeid, roletype, paydate),
 foreign key (employeeid) references employee(employeeid));
 
-insert into employee values (1000, 'Fahad',  'Hayat',    120000345, 6130000000, 'Hunt Club Ave', 11, 'Ottawa', 'Ontario', 'Canada', 'K3D1F3', 'TD Bank',     211, 234, 434343);
-insert into employee values (1002, 'Troy',   'Havelock', 140764377, 6131111111, 'Bank St',       21, 'Ottawa', 'Ontario', 'Canada', 'T5A1R7', 'TD Bank',     211, 235, 675543);
-insert into employee values (1003, 'Mir',    'Abbas',    839426234, 6132222222, 'Lib Rd',        55, 'Ottawa', 'Ontario', 'Canada', 'R5T6O7', 'Scotia Bank', 213, 678, 328479);
-insert into employee values (1004, 'Yunwei', 'Li',       345435433, 6133333333, 'Belle St',      78, 'Ottawa', 'Ontario', 'Canada', 'T3A9R0', 'RBC',         212, 998, 435643);
+insert into employee values (1000, 'Fahad',  'Hayat',    '120000345', '1', '613', '0000000', null, 'Hunt Club Ave', 11, 'Ottawa', 'Ontario', 'Canada', 'K3D1F3', 'TD Bank',     211, 234, 434343);
+insert into employee values (1002, 'Troy',   'Havelock', '140764377', '1', '613', '1111111', null, 'Bank St',       21, 'Ottawa', 'Ontario', 'Canada', 'T5A1R7', 'TD Bank',     211, 235, 675543);
+insert into employee values (1003, 'Mir',    'Abbas',    '839426234', '1', '613', '2222222', null, 'Lib Rd',        55, 'Ottawa', 'Ontario', 'Canada', 'R5T6O7', 'Scotia Bank', 213, 678, 328479);
+insert into employee values (1004, 'Yunwei', 'Li',       '345435433', '1', '613', '3333333', null, 'Belle St',      78, 'Ottawa', 'Ontario', 'Canada', 'T3A9R0', 'RBC',         212, 998, 435643);
 
-insert into employmentdetails values(1000, 'TA',      'Part-Time', 'Term',       '2017-01-01', '2017-04-30', 500);
-insert into employmentdetails values(1000, 'RA',      'Part-Time', 'Term',       '2017-01-01', '2017-04-30', 700);
-insert into employmentdetails values(1002, 'Faculty', 'Full-Time', 'Continuing', '2016-04-01', null,         2000);
-insert into employmentdetails values(1003, 'Staff',   'Full-Time', 'Continuing', '2015-01-01', null,         1000);
-insert into employmentdetails values(1003, 'RA',      'Part-Time', 'Term',       '2017-02-01', '2017-09-30', 700);
-insert into employmentdetails values(1003, 'TA',      'Part-Time', 'Term',       '2016-12-01', '2017-03-30', 500);
-insert into employmentdetails values(1004, 'TA',      'Part-Time', 'Term',       '2016-11-01', '2017-08-30', 500);
-insert into employmentdetails values(1004, 'Staff',   'Part-Time', 'Continuing', '2016-11-01', null,         1000);
+insert into employmentdetails values(1000, 'TA',      'Part-Time', 'Term',       '2017-01-01', '2017-04-30', 500, 1);
+insert into employmentdetails values(1000, 'RA',      'Part-Time', 'Term',       '2017-01-01', '2017-04-30', 700, 1);
+insert into employmentdetails values(1002, 'Faculty', 'Full-Time', 'Continuing', '2016-04-01', null,         2000, 1);
+insert into employmentdetails values(1003, 'Staff',   'Full-Time', 'Continuing', '2015-01-01', null,         1000, 1);
+insert into employmentdetails values(1003, 'RA',      'Part-Time', 'Term',       '2017-02-01', '2017-09-30', 700, 1);
+insert into employmentdetails values(1003, 'TA',      'Part-Time', 'Term',       '2016-12-01', '2017-03-30', 500, 1);
+insert into employmentdetails values(1004, 'TA',      'Part-Time', 'Term',       '2016-11-01', '2017-08-30', 500, 1);
+insert into employmentdetails values(1004, 'Staff',   'Part-Time', 'Continuing', '2016-11-01', null,         1000, 1);
--- TeamOOPS_cuNICS/listemployeescontrol.cpp
+++ TeamOOPS_cuNICS/listemployeescontrol.cpp
@@ -10,7 +10,7 @@
 #include <QSqlQuery>
 #include <QSqlRecord>
 #include <QString>
-
+#include <iostream>
 using namespace std;
 
 ListEmployeesControl::ListEmployeesControl()
@@ -27,31 +27,45 @@ int ListEmployeesControl::setFilter(User* filter)
 int ListEmployeesControl::getAllEmployees()
 {
     employees = new QList<User*>();
-    QSqlQuery query("SELECT * FROM employee");
+//    QSqlQuery query("SELECT employeeid, firstname, lastname, countrycode, areacode, localnumber, extension, street, "
+//                    "streetnumber, city, province, country, postalcode, accountnumber, banknumber, branchnumber, "
+//                    "bankname, sinnumber"
+//                    " FROM employee");
+    QSqlQuery query("SELECT * FRROM employee");
+    if(query.first()){
+        cout << "t" << endl;
+    }
 
-    int indexEmployeeID = query.record().indexOf("employeeid");
-    int indexFName = query.record().indexOf("firstname");
-    int indexLName = query.record().indexOf("lastname");
-    int indexCountryCode = query.record().indexOf("phonenumber");
-    int indexStreet = query.record().indexOf("street");
-    int indexStreetNumber = query.record().indexOf("streetnumber");
-    int indexCity = query.record().indexOf("city");
-    int indexProvince = query.record().indexOf("province");
-    int indexCountry = query.record().indexOf("country");
-    int indexPostalCode = query.record().indexOf("postalcode");
+    int indexEmployeeID =    query.record().indexOf("employeeid");
+    int indexFName =         query.record().indexOf("firstname");
+    int indexLName =         query.record().indexOf("lastname");
+    int indexCountryCode =   query.record().indexOf("countrycode");
+    int indexAreaCode =      query.record().indexOf("areacode");
+    int indexLocalNumber =   query.record().indexOf("localnumber");
+    int indexExtension =     query.record().indexOf("extension");
+    int indexStreet =        query.record().indexOf("street");
+    int indexStreetNumber =  query.record().indexOf("streetnumber");
+    int indexCity =          query.record().indexOf("city");
+    int indexProvince =      query.record().indexOf("province");
+    int indexCountry =       query.record().indexOf("country");
+    int indexPostalCode =    query.record().indexOf("postalcode");
     int indexAccountNumber = query.record().indexOf("accountnumber");
-    int indexBankNumber = query.record().indexOf("banknumber");
-    int indexBranchNumber = query.record().indexOf("branchnumber");
-    int indexBankName = query.record().indexOf("bankname");
-    int indexSinNumber = query.record().indexOf("sinnumber");
+    int indexBankNumber =    query.record().indexOf("banknumber");
+    int indexBranchNumber =  query.record().indexOf("branchnumber");
+    int indexBankName =      query.record().indexOf("bankname");
+    int indexSinNumber =     query.record().indexOf("sinnumber");
     int i = 0;
     while (query.next()){
+
         employees->append(
                     new Employee(
                                  query.value(indexFName).toString().toStdString(),
                                  query.value(indexLName).toString().toStdString(),
                                  query.value(indexEmployeeID).toInt(),
-                                 new PhoneNumber(),
+                                 new PhoneNumber(query.value(indexCountryCode).toInt(),
+                                                 query.value(indexAreaCode).toInt(),
+                                                 query.value(indexLocalNumber).toInt(),
+                                                 query.value(indexExtension).toInt()),
                                  new Address(query.value(indexStreet).toString().toStdString(),
                                              query.value(indexStreetNumber).toInt(),
                                              query.value(indexCity).toString().toStdString(),
@@ -67,9 +81,12 @@ int ListEmployeesControl::getAllEmployees()
                                  query.value(indexSinNumber).toInt()
                                  )
                         );
+        cout << query.value(indexFName).toString().toStdString();
         QString qstr = QString::fromStdString(employees->at(i)->toString());
+        qDebug() << employees->size();
         qDebug() << qstr << endl;
         i++;
+
     }
     return 0;
 }
